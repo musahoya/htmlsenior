ba9796fa8179e342aa684848573114d8782ad26d
musahoya <mrcpacta@naver.com>
2026-08-16 16:44:57 +0900

    Connect commute app to Netlify backend

--- commute-test/config.js
+++ commute-test/config.js
@@ -1,3 +1,2 @@
-// Netlify 사이트를 배포한 뒤 발급받은 주소로 교체하세요.
-window.COMMUTE_API_URL = 'https://YOUR-SITE.netlify.app/.netlify/functions';
-
+// Netlify Function 공개 주소입니다. 카카오 API 키는 Netlify 환경변수에만 저장됩니다.
+window.COMMUTE_API_URL = 'https://busan-commute-api.netlify.app/.netlify/functions';
